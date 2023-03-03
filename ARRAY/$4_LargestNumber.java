c83630507554d9417736bc45ea16a9625d496e44
manish9427 <manish119427@gmail.com>
2023-03-03 15:40:43 +0530

    largest Element

--- ARRAY/$4_LargestNumber.java
+++ ARRAY/$4_LargestNumber.java
@@ -1,9 +1,15 @@
 public class $4_LargestNumber {
     public static void main(String[] args){
         int arr[]={1,2,3,4,5};
+        int large=arr[0];
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+"");
+            System.out.println(arr[i]+" ");
         }
+        for(int i=0;i<arr.length;i++){
+            if(large<arr[i]){
+                large=arr[i];
+            }
+        }System.out.print("largest element "+ large);
     }
 }
 // import java.util.Scanner;
