ca953effac9acc2a8fbc2dd2bb505d4835a196b6
manish9427 <manish119427@gmail.com>
2023-03-06 16:43:19 +0530

    balanced parenthesi

---
+++ STRING/$25_BalancedParenthesis.java
@@ -0,0 +1,37 @@
+// {}()[]if these are equall in number means balanced parenthesis
+package STRING;
+
+import java.util.*;
+public class $25_BalancedParenthesis {
+
+    public class BalancedParenthesis {
+        public static boolean isBalanced(String str) {
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c == '('||c=='{'||c=='[') {
+                    count++;
+                } else if (c == ')'||c=='}'||c==']') {
+                    count--;
+                }
+                if (count < 0) {
+                    return false;
+                }
+            }
+            return count == 0;
+        }
+    
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter the String: ");
+            String str=sc.nextLine();
+
+            if(isBalanced(str)){
+                System.out.println("Balanced");
+            }else{
+                System.out.println("Not Balanced");
+            }
+        }
+    }
+    
+}
