490d98d9bb60af186f0c0f80e219878eb91a7f5a
manish9427 <manish119427@gmail.com>
2023-03-07 17:16:03 +0530

    Copy String

---
+++ STRING/$4_CopyString.java
@@ -0,0 +1,28 @@
+package STRING;
+
+// import java.util.*;
+// public class $4_CopyString {
+    
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter the String: ");
+//         String str=sc.nextLine();
+//         System.out.println("Original String: "+str);
+//         String Copy=str;
+//         System.out.println("Copied String: "+Copy);
+//     }
+// }
+
+// ---------------------------valueOf()-------------------------String.valueOf()
+import java.util.*;
+public class $4_CopyString {
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String str=sc.nextLine();
+        System.out.println("Original String: "+str);
+        String Copy=String.valueOf(str);
+        System.out.println("Copied String: "+Copy);
+    }
+}
