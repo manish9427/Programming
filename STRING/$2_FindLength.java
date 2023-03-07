1c9ac20899629c83ce71cc13ba8866dd0bda4bd3
manish9427 <manish119427@gmail.com>
2023-03-07 16:02:36 +0530

    length+

--- STRING/$2_FindLength.java
+++ STRING/$2_FindLength.java
@@ -2,17 +2,17 @@ package STRING;
 import java.util.*;
 public class $2_FindLength {
 
+
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the string: ");
-
         String str=sc.nextLine();
-            int len=0;
-            char [] ch=str.toCharArray();
-            for (char a:ch){
-                len++;
-            }
-            System.out.println("output: "+len);
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            count++;
+        }
+        System.out.print("length "+count);
     }
 }
+
 
