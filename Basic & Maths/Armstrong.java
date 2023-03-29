2ef93260ee646c75df097e8bb0b76e1c49cdb92e
manish9427 <manish119427@gmail.com>
2023-03-29 22:57:42 +0530

    armstrong

--- Basic & Maths/Armstrong.java
+++ Basic & Maths/Armstrong.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Armstrong{
     public static void main(String args[]){
         Scanner sc=new Scanner (System.in);
-        System.out.println("Enter the Number: ");
+        System.out.print("Enter the Number: ");
         int number = sc.nextInt();
         if (isArmstrong(number)) {
             System.out.println(number + " is an Armstrong number");
@@ -11,15 +11,19 @@ public class Armstrong{
         }
 
     }
-    public static boolean isArmstrong(int number) {
-        int sum = 0;
-        int temp = number;
-        while (temp > 0) {
-            int digit = temp % 10;
-            sum += digit * digit * digit;
-            temp /= 10;
-        }
-        return sum == number;
-    }
-    
+    public static boolean isArmstrong(int n) {  
+        int digits=0, last=0, sum=0;   
+        int temp=n;   
+        while(temp>0){   
+            temp = temp/10;   
+            digits++;   
+        }   
+        temp = n;   
+        while(temp>0)   {
+            last = temp % 10;   
+            sum +=  (Math.pow(last, digits));   
+            temp = temp/10;   
+        }  
+        return n == sum; 
+    }   
 }
