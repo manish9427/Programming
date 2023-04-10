8690e13946b359c776e8add741fe5e7e4a324866
manish9427 <manish119427@gmail.com>
2023-04-10 13:03:26 +0530

    count words frequency

---
+++ Programs/HashMap/WordFrequencyCounter.java
@@ -0,0 +1,31 @@
+package Programs.HashMap;
+
+
+import java.util.*;
+
+public class WordFrequencyCounter {
+    public static void main(String[] args) {
+        // Create a hash map to store word frequencies
+        Map<String, Integer> wordFreq = new HashMap<>();
+        
+        // Sample text
+        String text = "the quick brown fox jumps over the lazy dog";
+        
+        // Split text into words and count their frequency
+        String[] words = text.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (wordFreq.containsKey(word)) {
+                wordFreq.put(word, wordFreq.get(word) + 1);
+            } else {
+                wordFreq.put(word, 1);
+            }
+        }
+        
+        // Print out word frequencies
+        System.out.println("Word frequencies:");
+        for (Map.Entry<String, Integer> entry : wordFreq.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
