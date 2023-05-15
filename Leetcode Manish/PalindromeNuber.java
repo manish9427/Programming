c7a256c537fdd20ac5e9579fe823aadc9eeb6f56
manish9427 <manish119427@gmail.com>
2023-05-15 11:41:10 +0530

    new

---
+++ Leetcode Manish/PalindromeNuber.java
@@ -0,0 +1,30 @@
+/*
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+ */
+
+
+public class PalindromeNuber {
+    public static void main(String args[]){
+        int x=121;
+
+        if(isPalindrome(x)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("not a palindrome");
+        }
+    }
+    public static boolean isPalindrome(int x) {
+        String str=String.valueOf(x);
+        String  rev="";
+        for(int i=0;i<str.length();i++){
+             rev=str.charAt(i)+rev;
+        }
+        if(rev.equals(str)){
+            return true;
+        }else{
+            return false;
+        }
+    } 
+}
