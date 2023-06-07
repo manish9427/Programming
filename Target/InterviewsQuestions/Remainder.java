b381454a2606298fd0a30156bbcb4d4773961a37
manish9427 <manish119427@gmail.com>
2023-06-07 10:38:53 +0530

    new

--- Target/InterviewsQuestions/Remainder.java
+++ Target/InterviewsQuestions/Remainder.java
@@ -5,13 +5,32 @@
 // output
 // result = 0
 
+import java.util.*;
 public class Remainder {
     public static void main(String args[]){
-        int dividend=1;
-        int divisor=5;
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Input");
+        System.out.print("dividend = ");
+        int dividend=sc.nextInt();
+        System.out.print("divisor = ");
+        int divisor=sc.nextInt();
         int remainder=dividend%divisor;
         int quotient=dividend/divisor;
+        System.out.println("Output");
         System.out.println("remainder =  "+remainder);
         System.out.println("quotient = "+quotient);
     }
 }
+
+// === finding quotient and reminder ====================================
+
+// public class Remainder {
+//     public static void main(String args[]){
+//         int dividend=1;
+//         int divisor=5;
+//         int remainder=dividend%divisor;
+//         int quotient=dividend/divisor;
+//         System.out.println("remainder =  "+remainder);
+//         System.out.println("quotient = "+quotient);
+//     }
+// }
