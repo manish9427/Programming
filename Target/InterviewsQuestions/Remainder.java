86cf3a62359849fc8ea4cd161432500646d4f845
manish9427 <manish119427@gmail.com>
2023-06-07 10:29:42 +0530

    remainder

---
+++ Target/InterviewsQuestions/Remainder.java
@@ -0,0 +1,17 @@
+// find the remainder without using % and /
+// input
+// dividend = 15
+// divisior = 5
+// output
+// result = 0
+
+public class Remainder {
+    public static void main(String args[]){
+        int dividend=1;
+        int divisor=5;
+        int remainder=dividend%divisor;
+        int quotient=dividend/divisor;
+        System.out.println("remainder =  "+remainder);
+        System.out.println("quotient = "+quotient);
+    }
+}
