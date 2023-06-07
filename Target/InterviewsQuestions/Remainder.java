293e0837953b8fbdb8a1d52f53a5a41210f84016
manish9427 <manish119427@gmail.com>
2023-06-07 11:36:44 +0530

    remainder

--- Target/InterviewsQuestions/Remainder.java
+++ Target/InterviewsQuestions/Remainder.java
@@ -5,22 +5,61 @@
 // output
 // result = 0
 
-import java.util.*;
-public class Remainder {
+
+// === finding quotient and reminder without using % and / ====================================
+
+import java.util.Scanner;
+public class Remainder{
     public static void main(String args[]){
-        Scanner sc=new Scanner (System.in);
+        Scanner sc=new Scanner(System.in);
         System.out.println("Input");
         System.out.print("dividend = ");
         int dividend=sc.nextInt();
         System.out.print("divisor = ");
         int divisor=sc.nextInt();
-        int remainder=dividend%divisor;
-        int quotient=dividend/divisor;
+        int remeinder = findRemainder(dividend,divisor);
+        int quotient= findQuoitent(dividend,divisor);
+
         System.out.println("Output");
-        System.out.println("remainder =  "+remainder);
-        System.out.println("quotient = "+quotient);
+        System.out.println("Remeinder = "+remeinder);
+        System.out.println("Quotient = "+quotient);
+
+    }
+
+    public static int findRemainder(int dividend,int divisior){
+        while(dividend>=divisior){
+            dividend=dividend-divisior;
+        }return dividend;
+    }
+    public static int findQuoitent(int dividend,int divisior){
+        int quotient=0;
+        while(dividend>=divisior){
+            dividend=dividend-divisior;
+            quotient++;
+        }return quotient;
     }
 }
+
+
+
+// === finding quotient and reminder with user input ====================================
+
+// import java.util.*;
+// public class Remainder {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner (System.in);
+//         System.out.println("Input");
+//         System.out.print("dividend = ");
+//         int dividend=sc.nextInt();
+//         System.out.print("divisor = ");
+//         int divisor=sc.nextInt();
+//         int remainder=dividend%divisor;
+//         int quotient=dividend/divisor;
+//         System.out.println("Output");
+//         System.out.println("remainder =  "+remainder);
+//         System.out.println("quotient = "+quotient);
+//     }
+// }
 
 // === finding quotient and reminder ====================================
 
