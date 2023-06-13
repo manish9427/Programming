105a6f922984a8a7996101baef383ed9bf462e86
manish9427 <manish119427@gmail.com>
2023-06-13 12:03:39 +0530

    new

---
+++ Target/LeetcodePlayground/RemoveUnwantedChar.js
@@ -0,0 +1,32 @@
+/**
+ * Given an array [“123f”, “1dsa12”, “1212ds”, “65fd”, “sadfa”, “asdasd”]
+ * Each item can contain 0-9, a-z, A-Z where a-z, A-Z characters are unwanted
+ * Sum of all the numbers after removing all the unwanted characters 123+112+1212+65
+ **/
+const array = ["123f", "1dsa12", "1212ds", "65fd", "sadfa", "asdasd"];
+
+function sumOfNumbers(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    let number = "";
+
+    for (let j = 0; j < item.length; j++) {
+      const char = item[j];
+
+      if (/[0-9]/.test(char)) {
+        number += char;
+      }
+    }
+
+    if (number !== "") {
+      sum += parseInt(number);
+    }
+  }
+
+  return sum;
+}
+
+const result = sumOfNumbers(array);
+console.log(result); // Output: 1512
