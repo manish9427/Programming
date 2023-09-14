651f2064e8c37214701bef8d327748c69e796b70
manish9427 <manish119427@gmail.com>
2023-09-14 14:00:22 +0530

    new

--- Basic & Maths/fibonacci.java
+++ Basic & Maths/fibonacci.java
@@ -1,29 +1,30 @@
-    
-import java.util.*;
-public class Fibonacci{
-
+class Fibonacci{
     public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    //brute force
-    //logic
-    // first=0
-    // second=1
-    // third=first+second;
-    System.out.print("Enter first Number: ");
-    int first=sc.nextInt();
-    System.out.print("Enter second Number: ");
-    int second=sc.nextInt();
-    System.out.print("Enter the number of terms: ");
-    int n=sc.nextInt();
-    n=n-2;
-    int third=0;
-    while(n>0){
-        first=second;
-        second=third;
-        third=first+second;
-        System.out.println(third);
-        n--;
+        int n=5;
+         
+        int[] ans = Solution(n);
 
+        for(int i=0;i<ans.length;i++){
+            System.out.print (ans[i]+" ");
+        }
     }
+
+
+    public static int[] Solution(int n){
+        int[] ans= new int [n];
+
+        if(n>=1){
+            ans[0]=0;
+        }
+
+        if(n>=2){
+            ans[1]=1;
+        }
+
+        for(int i=2;i<n;i++){
+            ans[i]=ans[i-1]+ans[i-2];
+        }
+
+        return ans;
     }
 }
