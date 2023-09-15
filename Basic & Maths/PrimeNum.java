556a52ab96764262e2c95a39b64f001f1fc11d99
manish9427 <manish119427@gmail.com>
2023-09-15 20:42:29 +0530

    new

---
+++ Basic & Maths/PrimeNum.java
@@ -0,0 +1,13 @@
+class PrimeNum{
+    public static void main(String args[]){
+        int num = 10;
+        if(Solution(num)){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+    }
+    public static boolean Solution(int num){
+        return false;
+    }
+}
