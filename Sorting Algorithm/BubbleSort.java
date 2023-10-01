634e5941dceea049b0e419c25d30feee0e2ab315
manish9427 <manish119427@gmail.com>
2023-10-01 12:43:48 +0530

    new

--- Sorting Algorithm/BubbleSort.java
+++ Sorting Algorithm/BubbleSort.java
@@ -7,12 +7,32 @@
  * O(1)
  */
 
+ /*
+  * pseudo code
+  for i from 1 to N
+  for j from 0 to N-1
+  if(arr[j]>arr[j+1])
+  swap(arr[j],arr[j+1])
+  */
+
 import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String args[]){
         int[] arr = {-8, 10, 15, -6, -8, 10, 12};
-        Arrays.sort(arr);
+        Solution(arr);
         System.out.println(Arrays.toString(arr));
+    }
+    public static void Solution(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    // swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
     }
 }
