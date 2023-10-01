0643ae6bee96372dcd98bc6562d6b968a313bcae
manish9427 <manish119427@gmail.com>
2023-10-01 12:09:46 +0530

    new

--- Sorting Algorithm/BubbleSort.java
+++ Sorting Algorithm/BubbleSort.java
@@ -1,5 +1,18 @@
+/*
+ * time complexity 
+ * O(n^2) for worst case when array is sorted in decending order
+ * O(n) when array is sorted
+ * 
+ * Space complexity
+ * O(1)
+ */
+
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String args[]){
-        System.out.println("hello");
+        int[] arr = {-8, 10, 15, -6, -8, 10, 12};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
