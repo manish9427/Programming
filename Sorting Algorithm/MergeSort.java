2b4ec941d70d7990589445f5e27bc6ebaaa6c303
manish9427 <manish119427@gmail.com>
2023-10-04 09:23:38 +0530

    new

--- Sorting Algorithm/MergeSort.java
+++ Sorting Algorithm/MergeSort.java
@@ -1,5 +1,16 @@
-class MergeSort{
+import java.util.*;
+public class MergeSort{
     public static void main(String args[]){
-        System.out.println("hello");
+        int[] arr={-8,4,19,10,-15,20,10};
+        Solution(arr);
+        // Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        // for(int i : arr){
+        //     System.out.print( i + " ");
+        // }
+    }
+
+    public static void Solution(int arr[]){
+        
     }
 }
