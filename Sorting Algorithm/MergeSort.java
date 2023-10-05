ab134dd14b86a68cad8f4acdd869e351a68a47c3
manish9427 <manish119427@gmail.com>
2023-10-05 10:34:45 +0530

    new

--- Sorting Algorithm/MergeSort.java
+++ Sorting Algorithm/MergeSort.java
@@ -1,16 +1,20 @@
+/*
+ * recursively divides the array in 2 , sort, recombine
+ * T.C => O(nlog(n))
+ * S.C => O(n)
+ */
+
+
 import java.util.*;
 public class MergeSort{
     public static void main(String args[]){
         int[] arr={-8,4,19,10,-15,20,10};
-        Solution(arr);
-        // Arrays.sort(arr);
+        MergeSortFun(arr);
         System.out.println(Arrays.toString(arr));
-        // for(int i : arr){
-        //     System.out.print( i + " ");
-        // }
     }
 
-    public static void Solution(int arr[]){
-        
+    public static void MergeSortFun(int arr[]){
+        int length=arr.length;
+        if(length<=1) return;
     }
 }
