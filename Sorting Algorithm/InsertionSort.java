2a286c0b444dd9644d046e9b7fd3e568f0f33728
manish9427 <manish119427@gmail.com>
2023-10-07 14:55:15 +0530

    new

---
+++ Sorting Algorithm/InsertionSort.java
@@ -0,0 +1,13 @@
+/*
+ * after comparing element tot the left shift hte element  to the right to make room to insert a value
+ * 
+ * Time Complexity
+ * Quadratic time O(n^2)
+ * small data set = decent
+ * large data set = bad
+ * 
+ * less steps than bubble sort 
+ * best case is O(n) compared to Selection Sort O(n^2)
+ * 
+ * Space Complexity
+ *  */
