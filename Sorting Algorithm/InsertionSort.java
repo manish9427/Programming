961dee620a32a86e2197f1df9ce014ee58b008b1
manish9427 <manish119427@gmail.com>
2023-10-07 15:12:01 +0530

    new

--- Sorting Algorithm/InsertionSort.java
+++ Sorting Algorithm/InsertionSort.java
@@ -11,3 +11,29 @@
  * 
  * Space Complexity
  *  */
+import java.util.*;
+ public class InsertionSort{
+    public static void main(String[] args){
+        int[] arr={-8,-4,5,7,6,8,2,-4,10,100};
+        // Arrays.sort(arr);
+        // for(int i : arr){
+        //     System.out.print(i+ " ");
+        // }
+        InsertionFun(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void InsertionFun(int[] arr){
+        // starting with index i=1;
+        for(int i=1;i<arr.length;i++){ // increment i with each iteration
+            int temp=arr[i];
+            int j=i-1;  // keep index of previous index;
+
+            while(j>=0 && arr[j]>temp){ // index j should not go beyond 0 if arr[j] is greater than temp
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
+ }
