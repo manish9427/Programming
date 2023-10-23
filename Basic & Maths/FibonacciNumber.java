3ecbee245d1859474da5f12467e7d6d2123c6f87
manish9427 <manish119427@gmail.com>
2023-10-23 10:51:40 +0530

    new

---
+++ Basic & Maths/FibonacciNumber.java
@@ -0,0 +1,54 @@
+// class Fibonacci{
+//     public static void main(String args[]){
+//         int n=5;
+         
+//         int[] ans = Solution(n);
+
+//         for(int i=0;i<ans.length;i++){
+//             System.out.print (ans[i]+" ");
+//         }
+//     }
+
+
+//     public static int[] Solution(int n){
+//         int[] ans= new int [n];
+
+//         if(n>=1){
+//             ans[0]=0;
+//         }
+
+//         if(n>=2){
+//             ans[1]=1;
+//         }
+
+//         for(int i=2;i<n;i++){
+//             ans[i]=ans[i-1]+ans[i-2];
+//         }
+
+//         return ans;
+//     }
+// }
+
+import java.util.*;
+
+public class FibonacciNumber {
+    public static void main(String args[]) {
+        int n = 5;
+        int first = 0;
+        int second = 1;
+
+        int[] ans = new int[n];
+        if(n>=1){
+            ans[0] = first;
+        }
+        if(n>=2){
+            ans[1] = second;
+        }
+
+        for (int i = 2; i < n; i++) {
+            ans[i] = ans[i - 1] + ans[i - 2];
+        }
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
