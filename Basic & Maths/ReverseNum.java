67fdc9c8f25f9c40013e4b6e92a34a30775df7f3
manish9427 <manish119427@gmail.com>
2023-10-23 09:48:38 +0530

    reverse Num

--- Basic & Maths/ReverseNum.java
+++ Basic & Maths/ReverseNum.java
@@ -1,17 +1,33 @@
-public class ReverseNum {
+// public class ReverseNum {
+//     public static void main(String args[]){
+//         int n=123;
+//         int result=Reverse(n);
+//         System.out.println(result);
+//     }
+//     public static int Reverse(int n){
+//         int ans=0;
+
+//         while(n!=0){
+//             int lastNum=n%10;
+//             ans= ans*10+lastNum;
+//             n=n/10;
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+public class ReverseNum{
     public static void main(String args[]){
         int n=123;
-        int result=Reverse(n);
-        System.out.println(result);
-    }
-    public static int Reverse(int n){
         int ans=0;
-
-        while(n!=0){
-            int lastNum=n%10;
-            ans= ans*10+lastNum;
+        while(n>0){
+            int rem=n%10;
+            ans=ans*10+rem;
             n=n/10;
         }
-        return ans;
+        System.out.println(ans);
     }
 }
