de807165db0115533667979d41d5dab3a739b2fc
manish9427 <manish119427@gmail.com>
2023-10-25 09:40:59 +0530

    new

--- ArrayList/Program.java
+++ ArrayList/Program.java
@@ -18,6 +18,32 @@ public class Program {
         fruits.remove(0);
         System.out.println(fruits.get(0));
 
+        // iterate
+        for(String i:fruits){
+            System.out.println(i);
+        }
+
+        //isEmpty
+        System.out.println(fruits.isEmpty());
         
+        //arraylist to array
+        String []  a= new String[fruits.size()];
+        a=fruits.toArray(a);
+        for(String i:a){
+            System.out.println(i);
+        }
+        
+        // indexOf
+        int index=fruits.indexOf("Mango");
+        System.out.println(index);
+
+        // contains()
+        System.out.println(fruits.contains("apple"));
+
+        // clear()
+        fruits.clear();
+
+            System.out.println(fruits);
+
     }
 }
