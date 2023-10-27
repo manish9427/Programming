dc1de95ad7f0fa55997a80a20c35219cd993bec5
manish9427 <manish119427@gmail.com>
2023-10-27 17:02:21 +0530

    new

--- SlidingWindow/MaximumSubArray.java
+++ SlidingWindow/MaximumSubArray.java
@@ -23,5 +23,31 @@ public class MaximumSubArray {
         int k=3;
 
         System.out.println(Arrays.toString(arr));
+
+        // for i to n
+        // length = n-k;
+
+        // arr[0]+arr[1]+arr[2];=>8
+        // arr[1]+arr[2]+arr[3];=>7
+        // arr[2]+arr[3]+arr[4];=>9
+        // arr[3]+arr[4]+arr[5];=>6
+
+        // store the value store
+        // if (store<current)
+        // restore the value to store
+
+
+        int maxSum=0;
+        for(int i=0;i<arr.length-k+1;i++){
+            int sum=arr[i]+arr[i+1]+arr[i+2];
+            // System.out.println(sum);
+            maxSum=Math.max(sum,maxSum);
+        }
+        System.out.println(maxSum);
+
     }
+
+    
 }
+// TC O(n)
+// SC O(1)
