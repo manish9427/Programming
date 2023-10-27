1334965c8c66d237d56c53155de3b0c89153ac01
manish9427 <manish119427@gmail.com>
2023-10-27 16:38:30 +0530

    new

---
+++ SlidingWindow/MaximumSubArray.java
@@ -0,0 +1,27 @@
+/*
+Input:
+
+Array: {2, 1, 5, 1, 3, 2}
+Window size k: 3
+Output:
+
+Maximum Subarray Sum: 9
+
+Input:
+
+Array: {4, 2, 10, 23, 8, 5, 1, 3}
+Window size k: 4
+Output:
+
+Maximum Subarray Sum: 39
+
+*/
+import java.util.Arrays;
+public class MaximumSubArray {
+    public static void main(String args[]){
+        int[] arr={2, 1, 5, 1, 3, 2};
+        int k=3;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
