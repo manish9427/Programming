5dffff2220ba40c1db4842ba9dc341265016c94e
manish9427 <manish119427@gmail.com>
2023-10-28 11:33:13 +0530

    new

--- Stack/ReduceString.java
+++ Stack/ReduceString.java
@@ -6,15 +6,26 @@ public class ReduceString {
         public static void main(String args[]){
             Stack<Character> stack =new Stack<>();
             String str="abcdabcde";
-            StringBuilder sb=new StringBuilder();
 
+            // StringBuilder sb=new StringBuilder();
+            // for(char c : str.toCharArray()){
+            //     if(!stack.contains(c)){
+            //         stack.push(c);
+            //         sb.append(c);
+            //     }
+            // }
+            // System.out.println(sb.toString());
+
+            String s = "";
             for(char c : str.toCharArray()){
                 if(!stack.contains(c)){
                     stack.push(c);
-                    sb.append(c);
+                    s=s+c;
                 }
             }
-            System.out.println(sb.toString());
+            System.out.println(s);
 
+            //Tc and sc O(n)
+            //Tc and sc O(n^2)
         }
     }
