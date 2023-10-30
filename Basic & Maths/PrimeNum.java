6e27a4e834d8a754e169c900149eaa57fb9a6592
manish9427 <manish119427@gmail.com>
2023-10-30 22:57:40 +0530

    new

--- Basic & Maths/PrimeNum.java
+++ Basic & Maths/PrimeNum.java
@@ -1,22 +1,43 @@
+// class PrimeNum{
+//     public static void main(String args[]){
+//         int num = 17;
+//         if(Solution(num)){
+//             System.out.println(num+" Prime");
+//         }else{
+//             System.out.println(num+" is Non Prime");
+//         }
+//     }
+//     public static boolean Solution(int num){  
+//         if(num<=1){
+//             return false;
+//         }
+
+//         for(int i=2;i*i<=num;i++){ 
+//             if(num%i==0){     
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
 class PrimeNum{
     public static void main(String args[]){
-        int num = 17;
-        if(Solution(num)){
-            System.out.println(num+" Prime");
-        }else{
-            System.out.println(num+" is Non Prime");
-        }
-    }
-    public static boolean Solution(int num){  
-        if(num<=1){
-            return false;
-        }
+        int n=17;
 
-        for(int i=2;i*i<=num;i++){ 
-            if(num%i==0){     
-                return false;
+        boolean isPrime=true;
+        for(int i=2;i*i<n;i++){
+            if(n%i==0){
+                isPrime=false;
             }
         }
-        return true;
+
+        if(isPrime){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
     }
 }
