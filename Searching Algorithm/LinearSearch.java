43b676657f22e70aad0802b057fc979da8f52510
manish9427 <manish119427@gmail.com>
2023-11-07 08:54:17 +0530

    new

--- Searching Algorithm/LinearSearch.java
+++ Searching Algorithm/LinearSearch.java
@@ -50,15 +50,19 @@
 
 class LinearSearch{
     public static void main(String args[]){
-        int[] arr = {1,2,3,4,-8,-10};
+        int[] arr = {1,2,3,4,-8,-10,-15,-15};
         int target = -15;
 
+        int index=-1;
         for(int i=0;i<arr.length;i++){
             if(arr[i]==target){
-                System.out.println(arr[i] +" is at index "+i);
-            }else{
-                System.out.println("Not Found "+ target);
-            }break;
+                System.out.println(i+" ");
+            }
+            index=1;
+        }
+
+        if(index==-1){
+            System.out.println("Not found");
         }
     }
 }
