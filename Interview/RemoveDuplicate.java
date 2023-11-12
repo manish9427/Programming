be1bab20485633f192b30c5e955ff66c33441387
manish9427 <manish119427@gmail.com>
2023-11-12 11:01:37 +0530

    new

--- Interview/RemoveDuplicate.java
+++ Interview/RemoveDuplicate.java
@@ -1,27 +1,60 @@
-import java.util.*;
-public class RemoveDuplicate {
-    public static void main(String args[]){
-        int[] arr = {1, 7, 1, 8, 4, 9, 7, 3};
-        // 1, 7, 8, 4, 9, 3 --> Output
+// import java.util.*;
+// public class RemoveDuplicate {
+//     public static void main(String args[]){
+//         int[] arr = {1, 7, 1, 8, 4, 9, 7, 3};
+//         // 1, 7, 8, 4, 9, 3 --> Output
 
+//         int[] ans = Solution(arr);
+//         Arrays.sort(ans);
+//         System.out.println(Arrays.toString(ans));
+//     }
+
+//     public static int[] Solution(int[] arr){
+//         int n = arr.length;
+//         Stack<Integer> st = new Stack<>();
+
+//         for (int i = 0; i < n; i++){
+//             if (!st.contains(arr[i])){
+//                 st.push(arr[i]);
+//             }
+//         }
+
+//         int[] ans = new int[st.size()];
+//         for (int i = ans.length - 1; i >= 0; i--) {
+//             ans[i] = st.pop();
+//         }
+//         return ans;
+//     }
+// }
+
+
+import java.util.*;
+public class RemoveDuplicate{
+    public static void main(String args[]){
+        int[] arr = {1,2,1,3,1,4,2,5,0};
         int[] ans = Solution(arr);
+        // Solution(arr);
         Arrays.sort(ans);
         System.out.println(Arrays.toString(ans));
     }
 
     public static int[] Solution(int[] arr){
-        int n = arr.length;
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st =new Stack <>();
 
-        for (int i = 0; i < n; i++){
-            if (!st.contains(arr[i])){
+        for(int i=0;i<arr.length;i++){
+            if(!st.contains(arr[i])){
                 st.push(arr[i]);
             }
         }
 
-        int[] ans = new int[st.size()];
-        for (int i = ans.length - 1; i >= 0; i--) {
-            ans[i] = st.pop();
+        // while(!st.isEmpty()){
+        //     System.out.println(st.peek());
+        //     st.pop();
+        // }
+
+        int ans[] =new int[st.size()];
+        for(int i=ans.length-1;i>=0;i--){
+            ans[i]=st.pop();
         }
         return ans;
     }
