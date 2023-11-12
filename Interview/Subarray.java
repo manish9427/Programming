7a8f7fa89027ee74cb4b0f7279064546b9fb6d92
manish9427 <manish119427@gmail.com>
2023-11-12 09:06:51 +0530

    new

--- Interview/Subarray.java
+++ Interview/Subarray.java
@@ -1,16 +1,30 @@
-public class Subarray {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
-        int length = arr.length;
+// public class Subarray {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4};
+//         int length = arr.length;
 
-        for (int start = 0; start < length; start++) {
-            for (int end = start; end < length; end++) {
-                for (int i = start; i <= end; i++) {
-                    System.out.print(arr[i] + " ");
+//         for (int start = 0; start < length; start++) {
+//             for (int end = start; end < length; end++) {
+//                 for (int i = start; i <= end; i++) {
+//                     System.out.print(arr[i] + " ");
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//     }
+// }
+
+
+public class Subarray{
+    public static void main(String args[]){
+        int[] arr ={1,2,3,4};
+        for (int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(k + " ");
                 }
-                System.out.println();
+                System.out.print("\n");
             }
         }
     }
 }
-
--- TwoPointer/NextGreaterElement.java
+++ TwoPointer/NextGreaterElement.java
@@ -5,32 +5,54 @@ Next Greater Elements:
 5 10 10 -1 -1
  */
 
-import java.util.Arrays;
-public class NextGreaterElement{
-    public static void main(String args[] ){
-        int[] arr ={1, 5 , 7 , 10 , -50};
+// import java.util.Arrays;
+// public class NextGreaterElement{
+//     public static void main(String args[] ){
+//         int[] arr ={1, 5 , 7 , 10 , -50};
 
-        int[] solution = Solution(arr) ;
-        System.out.println(Arrays.toString(solution));
-    }
+//         int[] solution = Solution(arr) ;
+//         System.out.println(Arrays.toString(solution));
+//     }
 
-    public static int[] Solution (int[] arr){
-        int n=arr.length;
-        int[]  result = new int [n];
+//     public static int[] Solution (int[] arr){
+//         int n=arr.length;
+//         int[]  result = new int [n];
 
-        for(int i=0;i<n;i++){
-            result[i] = -1;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]>arr[i]){
-                    result[i]=arr[j];
-                }break;
-            }
-        }
-        return result;
+//         for(int i=0;i<n;i++){
+//             result[i] = -1;
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[j]>arr[i]){
+//                     result[i]=arr[j];
+//                 }break;
+//             }
+//         }
+//         return result;
 
-    }
-}
+//     }
+// }
 
 
 // tc= O(n^2)
 // sc=O(1)
+
+import java.util.Arrays;
+public class NextGreaterElement{
+    public static void main(String args[]){
+        int[] arr={4,8,7,6,9,4,3};
+        int[] ans=FindGreater(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] FindGreater(int[] arr){
+        int ans[] = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=-1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>arr[i]){
+                    ans[i]=arr[j];
+                }break;
+            }
+        }
+        return ans;
+    }
+}
