84f212c26417f9c42d08db17a57814242baa1ffc
manish9427 <manish119427@gmail.com>
2023-11-12 09:14:35 +0530

    new

--- Interview/Substring.java
+++ Interview/Substring.java
@@ -25,24 +25,39 @@ SC => O(1)
 
 
 
-public class Substring {
-    public static void main(String[] args) {
-        String str = "abcd";
-        int length = str.length();
+// public class Substring {
+//     public static void main(String[] args) {
+//         String str = "abcd";
+//         int length = str.length();
 
-        // for (int i = 0; i < length; i++) {
-        //     for (int j = i + 1; j <= length; j++) {
-        //         String substring = str.substring(i, j);
-        //         System.out.println(substring);
-        //     }
-        // }
+//         // for (int i = 0; i < length; i++) {
+//         //     for (int j = i + 1; j <= length; j++) {
+//         //         String substring = str.substring(i, j);
+//         //         System.out.println(substring);
+//         //     }
+//         // }
 
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j <= length; j++) {
-                for (int k = i; k < j; k++) {
-                    System.out.print(str.charAt(k));
+//         for (int i = 0; i < length; i++) {
+//             for (int j = i + 1; j <= length; j++) {
+//                 for (int k = i; k < j; k++) {
+//                     System.out.print(str.charAt(k));
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//     }
+// }
+
+
+public class Substring{
+    public static void main(String args[]){
+        String str="abc";
+        for(int i=0;i<str.length();i++){
+            for(int j=i;j<str.length();j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(str.charAt(k)+" ");
                 }
-                System.out.println();
+                System.out.print("\n");
             }
         }
     }
