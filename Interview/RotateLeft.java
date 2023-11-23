a3ad380750a2378d2e40ed71075b9879020e0722
manish9427 <manish119427@gmail.com>
2023-11-23 12:00:10 +0530

    new

--- Interview/RotateLeft.java
+++ Interview/RotateLeft.java
@@ -7,14 +7,27 @@ public class RotateLeft {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void SolveRotateLeft(int[] arr , int k){
+    // public static void SolveRotateLeft(int[] arr , int k){
+    //     int n= arr.length;
+    //     for (int i = 0; i < k; i++) {
+    //         int temp = arr[0];
+    //         for (int j = 0; j < n - 1; j++) {
+    //             arr[j] = arr[j + 1];
+    //         }
+    //         arr[n - 1] = temp;
+    //     }
+    // }
+
+    public static void SolveRotateLeft(int[] arr,int k){
         int n= arr.length;
-        for (int i = 0; i < k; i++) {
-            int temp = arr[0];
-            for (int j = 0; j < n - 1; j++) {
-                arr[j] = arr[j + 1];
+        for(int i=0;i<k;i++){
+            int lastElement=arr[n-1];
+
+            for(int j=n-1;j>0;j--){
+                arr[j]=arr[j-1];
             }
-            arr[n - 1] = temp;
+
+            arr[0]=lastElement;
         }
     }
 }
