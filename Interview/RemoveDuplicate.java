634a33c5e8664614c1cb3a674975ec27e6cf742f
manish9427 <manish119427@gmail.com>
2023-11-27 19:02:10 +0530

    new

--- Interview/RemoveDuplicate.java
+++ Interview/RemoveDuplicate.java
@@ -28,51 +28,17 @@
 // }
 
 
-// import java.util.*;
-// public class RemoveDuplicate{
-//     public static void main(String args[]){
-//         int[] arr = {1,2,1,3,1,4,2,5,0};
-//         int[] ans = Solution(arr);
-//         // Solution(arr);
-//         Arrays.sort(ans);
-//         System.out.println(Arrays.toString(ans));
-//     }
 
-//     public static int[] Solution(int[] arr){
-//         Stack<Integer> st =new Stack <>();
-
-//         for(int i=0;i<arr.length;i++){
-//             if(!st.contains(arr[i])){
-//                 st.push(arr[i]);
-//             }
-//         }
-
-//         // while(!st.isEmpty()){
-//         //     System.out.println(st.peek());
-//         //     st.pop();
-//         // }
-
-//         int ans[] =new int[st.size()];
-//         for(int i=ans.length-1;i>=0;i--){
-//             ans[i]=st.pop();
-//         }
-//         return ans;
-//     }
-// }
 
 import java.util.*;
 class RemoveDuplicate{
     public static void main(String args[]){
 
         int [] arr= {1,2,1,2,4,2,8,2,10,6};
-
-        int [] ans=Solution(arr);
-        Arrays.sort(ans);
-
-        System.out.println(Arrays.toString(ans));
+        Solution(arr);
     }
 
-    public static int[] Solution(int [] arr){
+    public static void Solution(int [] arr){
         int n=arr.length;
         Stack<Integer> st =new Stack<>();
         for(int i=0;i<n;i++){
@@ -81,10 +47,10 @@ class RemoveDuplicate{
                 st.push(arr[i]);
             }
         }
-        int[] ans= new int[st.size()];
-        for(int i=ans.length-1;i>=0;i--){
-            ans[i]=st.pop();
+
+        while(!st.isEmpty()){
+            System.out.print(st.peek()+" ");
+            st.pop();
         }
-        return ans;
     }
 }
