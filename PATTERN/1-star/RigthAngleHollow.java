141fd8f3e334568d2b1fd64c552ac185462737f2
Manish <manish119427@gmail.com>
2024-02-17 18:31:44 +0530

    new

--- PATTERN/1-star/RigthAngleHollow.java
+++ PATTERN/1-star/RigthAngleHollow.java
@@ -1,8 +1,12 @@
 public class RigthAngleHollow {
     public static void main(String args[]){
         for(int i=1;i<=5;i++){
-            for(int j=0;j<i;j++){
-                System.out.print("*");
+            for(int j=1;j<=i;j++){
+                if(i==1||i==5||j==1||j==i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
