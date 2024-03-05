21712c827146f205c64882654e10da91fd8ee83c
Manish <manish119427@gmail.com>
2024-03-05 22:01:14 +0530

    new

--- PATTERN/1-star/TriangleHollow.java
+++ PATTERN/1-star/TriangleHollow.java
@@ -1,3 +1,19 @@
 public class TriangleHollow {
-    
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i*2+1;k++){
+                if(i==0||k==i*2||k==0||i==5-1){
+
+                    System.out.print("*");
+                }else{
+
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
