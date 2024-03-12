3b2ac77918dc5d7ff2a977c572ef25deead272d4
Manish <manish119427@gmail.com>
2024-03-12 22:03:30 +0530

    new

---
+++ PATTERN/1-star/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    public static void main(String rags[]){
+        int row=5;
+        int col=20;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
