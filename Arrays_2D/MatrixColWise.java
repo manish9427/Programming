db29e83be4c5ba0012c24687dbc375fea41a124c
Manish <manish119427@gmail.com>
2024-03-18 22:29:17 +0530

    new

--- Arrays_2D/MatrixColWise.java
+++ Arrays_2D/MatrixColWise.java
@@ -10,7 +10,7 @@ public class MatrixColWise {
         int add =1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                arr[i][j]=add;
+                arr[j][i]=add;
                 add++;
             }
         }
