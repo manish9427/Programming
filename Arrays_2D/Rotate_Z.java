40f34630dea6888d32ea4c520764612677a9271c
Manish <manish119427@gmail.com>
2024-03-19 23:23:54 +0530

    new

--- Arrays_2D/Rotate_Z.java
+++ Arrays_2D/Rotate_Z.java
@@ -17,5 +17,19 @@ public class Rotate_Z {
         System.out.print("Enter the Col: ");
         int n=sc.nextInt();
         int [][] arr =new int [m][n];
+        int num=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=num;
+                num++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
