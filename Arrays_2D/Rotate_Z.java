8d771daf73ab121419e3d1b2465d5bc71c752263
Manish <manish119427@gmail.com>
2024-03-19 23:19:39 +0530

    new

--- Arrays_2D/Rotate_Z.java
+++ Arrays_2D/Rotate_Z.java
@@ -8,8 +8,14 @@ Z - Rotate 90 anticlockwise
  256
  147
  */
+import java.util.*;
 public class Rotate_Z {
     public static void main(String args[]){
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Row: ");
+        int m=sc.nextInt();
+        System.out.print("Enter the Col: ");
+        int n=sc.nextInt();
+        int [][] arr =new int [m][n];
     }
 }
