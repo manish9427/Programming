71c8f5ca8bb883c15af4bc2343bf446db1b3c33e
Manish <manish119427@gmail.com>
2024-03-24 21:58:13 +0530

    new

--- Arrays_2D/Rotate_Z.java
+++ Arrays_2D/Rotate_Z.java
@@ -12,11 +12,11 @@ import java.util.*;
 public class Rotate_Z {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the Row: ");
+        System.out.print("Enter the row: ");
         int m=sc.nextInt();
-        System.out.print("Enter the Col: ");
+        System.out.print("Enter the row: ");
         int n=sc.nextInt();
-        int [][] arr =new int [m][n];
+        int [][] arr =new int[m][n];
         int num=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -24,12 +24,27 @@ public class Rotate_Z {
                 num++;
             }
         }
+        System.out.println("Input :-");
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                System.out.print(arr[i][j]+" ");
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+ " ");
             }
             System.out.println();
         }
-
+        int row = arr.length;
+        int col = arr[0].length;
+        int [][] newArray = new int[col][row];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                newArray[col-1-j][i]=arr[i][j];
+            }
+        }
+        System.out.println("Output :-");
+        for(int i=0;i<newArray.length;i++){
+            for(int j=0;j<newArray[0].length;j++){
+                System.out.print(newArray[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
