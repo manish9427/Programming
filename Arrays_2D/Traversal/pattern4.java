9bfcc6853a8ac975d123092d34168a64bcf96a46
Manish <manish119427@gmail.com>
2024-03-28 22:40:08 +0530

    new

---
+++ Arrays_2D/Traversal/pattern4.java
@@ -0,0 +1,50 @@
+/*
+741
+852
+963
+ */
+import java.util.Scanner;
+
+public class pattern4 {
+    public static void main(String args[]){
+         Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the Row: ");
+        int m=sc.nextInt();
+        System.out.print("Enter the Col: ");
+        int n=sc.nextInt();
+        int [][] arr =new int[m][n];
+        
+        System.out.print("Enter the Array First Number: ");
+        int num=sc.nextInt();
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j] = num;
+                num++;
+            }
+        }
+        System.out.println("Output:- ");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        int row = arr.length;
+        int col = arr[0].length;
+        int newArray[][] = new int[row][col]; 
+
+        for(int i=0;i<newArray.length;i++){
+            for(int j=0;j<newArray[i].length;j++){
+                newArray[i][j] =arr[i][col-1-j];
+            }
+        }
+        System.out.println("Input:- ");
+        for(int i=0;i<newArray.length;i++){
+            for(int j=0;j<newArray[0].length;j++){
+                System.out.print(newArray[i][j]+" ");
+            }
+            System.out.println();
+        }    
+    }
+}
