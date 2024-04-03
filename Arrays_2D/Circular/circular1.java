3ac38ef57b56e6b9c76ab0f2beccf98122de2b3c
Manish <manish119427@gmail.com>
2024-04-03 23:27:31 +0530

    new

--- Arrays_2D/Circular/circular1.java
+++ Arrays_2D/Circular/circular1.java
@@ -10,10 +10,12 @@ public class circular1 {
             num++;
         }
        }  
+       int top = 1;
+       int bottom = arr.length;
 
        for(int i=0;i<arr.length;i++){
         for(int j=0;j<arr.length;j++){
-            System.out.print(arr[i][j]);
+            System.out.print(arr[i][j]+" ");
         }
         System.out.println();
        }
