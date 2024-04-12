3c040253ec96f1fe5d350867a887935beffd6a23
Manish <manish119427@gmail.com>
2024-04-12 23:53:57 +0530

    new

--- StringBuilder/Example.java
+++ StringBuilder/Example.java
@@ -1,5 +1,16 @@
 public class Example {
     public static void main(String args[]){
-        
+        StringBuilder sb =new StringBuilder();
+        //append()
+        sb.append("Hello");
+        sb.append(" ");
+        sb.append("world");
+        sb.append("!");
+
+        //toString() --> Converting StringBuilder to String 
+
+        String result =sb.toString();
+        System.out.println(result);
+
     }
 }
