7a4f4bf73bff6d86515f4c50ab849e24fd21d831
Manish <manish119427@gmail.com>
2024-04-16 22:10:55 +0530

    new

--- HashTable/ExampleHastable.java
+++ HashTable/ExampleHastable.java
@@ -8,6 +8,11 @@ public class ExampleHastable {
         table.put("Mango",20);
         table.put("Grapes",30);
 
+        //get
+        System.out.println("What is the value of Apple: "+  table.get("Apple"));
+
+
+        // Iterage
         for(String Key : table.keySet()){
             int value = table.get(Key);
             System.out.println(Key + " : "+value);
