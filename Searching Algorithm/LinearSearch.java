a9dbf734104b1e31c9cc52fe1796a0ebb4cd23b2
Manish <manish119427@gmail.com>
2024-04-24 09:31:52 +0530

    new

--- Searching Algorithm/LinearSearch.java
+++ Searching Algorithm/LinearSearch.java
@@ -67,4 +67,19 @@
 //     }
 // }
 
-
+class LinearSearch{
+    public static void main(String args[]){
+        int [] arr = {1,2,3,3,3,4,5,6,7};
+        int target = 3;
+        boolean found =false;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                found =true;
+                System.out.println(i);
+            }
+        }
+        if(!found){
+            System.out.println("Not Found");
+        }
+    }
+}
