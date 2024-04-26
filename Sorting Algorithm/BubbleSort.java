7b04a4d580bb1bb72ee2f63bcb74414e8f72c7f1
Manish <manish119427@gmail.com>
2024-04-26 22:53:09 +0530

    new

--- Sorting Algorithm/BubbleSort.java
+++ Sorting Algorithm/BubbleSort.java
@@ -65,16 +65,16 @@ class BubbleSort{
     public static void main(String args[]){
         int[] arr = {1,4,2,9,3,1,2,3};
         for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length-1;j++){
+            for(int j=i+1;j<arr.length;j++){
                 if(arr[j]<arr[i]){
-                    int temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+                    int temp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
                 }
             }
         }
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
+            System.out.print(arr[i]+" ");
         }
     }
 }
