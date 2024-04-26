db70ad990bfde611d9b8fa0429730764ab351234
Manish <manish119427@gmail.com>
2024-04-26 22:43:08 +0530

    new

--- Sorting Algorithm/BubbleSort.java
+++ Sorting Algorithm/BubbleSort.java
@@ -38,24 +38,43 @@
 // }
 
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-public class BubbleSort {
+// public class BubbleSort {
+//     public static void main(String args[]){
+//         int[] arr = {-8, 10, 15, -6, -8, 10, 12};
+//         Solution(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+//     public static void Solution(int arr[]){
+//         for(int i=0;i<arr.length-1;i++){
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[j]<arr[i]){
+//                     int temp=arr[j];
+//                     arr[j]=arr[i];
+//                     arr[i]=temp;
+//                 }
+//                 System.out.println(Arrays.toString(arr));
+//             }
+//         }
+//     }
+// }
+
+
+class BubbleSort{
     public static void main(String args[]){
-        int[] arr = {-8, 10, 15, -6, -8, 10, 12};
-        Solution(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-    public static void Solution(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
+        int[] arr = {1,4,2,9,3,1,2,3};
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length-1;j++){
                 if(arr[j]<arr[i]){
-                    int temp=arr[j];
-                    arr[j]=arr[i];
-                    arr[i]=temp;
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
-                System.out.println(Arrays.toString(arr));
             }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
         }
     }
 }
