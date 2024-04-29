2947ce82d9ef4e6f68fa96bdb5b64a2fa2218778
Manish <manish119427@gmail.com>
2024-04-29 10:24:06 +0530

    new

--- Sorting Algorithm/InsertionSort.java
+++ Sorting Algorithm/InsertionSort.java
@@ -39,11 +39,29 @@
 //     }
 //  }
 
+// import java.util.Arrays;
+// class InsertionSort{
+//     public static void main(String args[]){
+//         int[] arr = {5,3,1,2,4};
+//         Arrays.sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
 import java.util.Arrays;
+
 class InsertionSort{
     public static void main(String args[]){
-        int[] arr = {5,3,1,2,4};
-        Arrays.sort(arr);
+        int arr[] = {12,11,13,5,6};
+        for(int i=1;i<arr.length;i++){
+            int j=i-1;
+            int temp=arr[i];
+            while(j>=0 && arr[j]>temp) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
         System.out.println(Arrays.toString(arr));
     }
 }
