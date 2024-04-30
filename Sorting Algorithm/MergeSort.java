274b91c901abbb15b2b58c79cd707eb5c4bf6aec
Manish <manish119427@gmail.com>
2024-04-30 11:03:21 +0530

    new

--- Sorting Algorithm/MergeSort.java
+++ Sorting Algorithm/MergeSort.java
@@ -91,15 +91,29 @@ class MergeSort{
         // }
 
         // Bubble Sort
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[i]){
-                    int temp =arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-                }
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[j]<arr[i]){
+        //             int temp =arr[i];
+        //             arr[i]=arr[j];
+        //             arr[j]=temp;
+        //         }
+        //     }
+        // }
+
+        // Insertion Sort
+        for(int i=1;i<arr.length;i++){
+            int temp =arr[i];
+            int j=i-1;
+
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
             }
+            arr[j+1]=temp;
         }
+
+
         System.out.println(Arrays.toString(arr));
     }
 }
