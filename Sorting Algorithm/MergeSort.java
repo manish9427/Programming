e402bf4e6938d0fb4097f7ef8904708285a62cdc
manish9427 <manish119427@gmail.com>
2024-05-03 23:55:38 +0530

    new

--- Sorting Algorithm/MergeSort.java
+++ Sorting Algorithm/MergeSort.java
@@ -76,44 +76,6 @@ import java.util.Arrays;
 class MergeSort{
     public static void main(String args[]){
         int arr[] = {3,2,8,7,2,1};
-
-        // Selection Sort
-        // for(int i=0;i<arr.length-1;i++){
-        //     int  min = i;
-        //     for(int j=i+1;j<arr.length;j++){
-        //         if(arr[j]<arr[min]){
-        //             min = j;
-        //         }
-        //     }
-        //     int temp = arr[min];
-        //     arr[min]=arr[i];
-        //     arr[i]=temp;
-        // }
-
-        // Bubble Sort
-        // for(int i=0;i<arr.length;i++){
-        //     for(int j=i+1;j<arr.length;j++){
-        //         if(arr[j]<arr[i]){
-        //             int temp =arr[i];
-        //             arr[i]=arr[j];
-        //             arr[j]=temp;
-        //         }
-        //     }
-        // }
-
-        // Insertion Sort
-        for(int i=1;i<arr.length;i++){
-            int temp =arr[i];
-            int j=i-1;
-
-            while(j>=0 && arr[j]>temp){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=temp;
-        }
-
-
-        System.out.println(Arrays.toString(arr));
+        
     }
 }
